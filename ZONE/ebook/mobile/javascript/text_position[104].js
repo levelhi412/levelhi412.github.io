63c586aeda8372417e5dbcaca39d525d22549a48
levelhi412 <levelhi@sina.com>
2020-09-20 02:21:31 +0800

    Site updated: 2020-09-20 02:20:04

---
+++ ZONE/ebook/mobile/javascript/text_position[104].js
@@ -0,0 +1,19 @@
+positionForPages[104]={page:104,positions:[]};<script>
+        document.querySelectorAll('.github-emoji')
+          .forEach(el => {
+            if (!el.dataset.src) { return; }
+            const img = document.createElement('img');
+            img.style = 'display:none !important;';
+            img.src = el.dataset.src;
+            img.addEventListener('error', () => {
+              img.remove();
+              el.style.color = 'inherit';
+              el.style.backgroundImage = 'none';
+              el.style.background = 'none';
+            });
+            img.addEventListener('load', () => {
+              img.remove();
+            });
+            document.body.appendChild(img);
+          });
+      </script>
